d219dd937a0658e3d515fb81ba8dc5f708550ce6
Wahab Ahmad <wahabahmadghori@gmail.com>
2023-09-14 19:38:54 +0500

    nodemon

--- src/router/chat_model.ts
+++ src/router/chat_model.ts
@@ -1,7 +1,7 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 export default (router: Router) => {
-  router.get("/chat", () => {
-    return [];
+  router.get("/chat", async (req: Request, res: Response) => {
+    return res.send("Message from ChatGPT 1234");
   });
 };
