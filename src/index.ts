ab4d809731db03b1179a82c29b376988414b0789
Wahab Ahmad <wahabahmadghori@gmail.com>
2023-09-14 18:41:12 +0500

    body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
-
-const server = express();
-
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+const app = express();
 const port = 3000;
 
-server
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app
   .get("/", (req, res) => {
     res.send({
       message: "This is first commit!",
@@ -16,6 +27,6 @@ server
     });
   });
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Application listening on port ${port}`);
 });
